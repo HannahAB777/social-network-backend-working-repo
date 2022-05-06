09939c839d562efcc3af4c9b8b8b8fd6c00fb89f
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-06 16:08:25 +0800

    users finished

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -33,15 +33,44 @@ router.get("/:id", async (req, res) => {
 });
 
 //put to update a user by its id
+router.post('/:id', async (req, res) =>{
+  await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then((updatedUser) => res.json(updatedUser))
+  .catch((err) => res.status(500).json(err));
+});
 
 //delete to remove a user by its _id
+router.delete("/:id", async (req, res) => {
+  await User.findByIdAndDelete({ _id: req.params.id })
+    .then((deletedUser) => res.json(deletedUser))
+    .catch((err) => res.status(500).json(err));
+});
 
 //bonus remove a users associated thoughts when deleted
 
-//api/users/:userId/friends/:friendsId
 
 //post to add a new friend to a users friends list
+router.post('/:userId/friends/:friendId', async (req, res) =>{
+  const user = await User.findByIdAndUpdate({_id: req.params.userId}, {$addToSet: { friends: req.params.friendId } }, {new: true});
+  const usersFriend = await User.findByIdAndUpdate({_id: req.params.friendId}, {$addToSet: { friends: req.params.userId } }, {new: true}).select("-__v");
+  if (res.status(200)) {
+    res.status(200).json([{user},{usersFriend}]);
+  } else {
+    console.log('Uh Oh, something went wrong');
+    res.status(500).json({ message: 'something went wrong' });
+  }
+});
 
 //delete to remove a friend from a users friend list
+router.delete('/:userId/friends/:friendId', async (req, res) =>{
+  const user = await User.findByIdAndUpdate({_id: req.params.userId}, {$pullAll: {friends: [req.params.friendId] }} , {new: true});
+  const usersFriend = await User.findByIdAndUpdate({_id: req.params.friendId}, {$pullAll: {friends: [req.params.userId] }}, {new: true}).select("-__v");
+    if (res.status(200)) {
+      res.status(200).json([{user},{usersFriend}]);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+});
 
 module.exports = router;
